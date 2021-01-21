53d5227ca3839f16fba2aec31ef2c28af42f1d07
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-01-21 21:23:56 +0200

    created template for add-breed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const path = require('path');
-const cats = require('./cats'); 
+const cats = require('./cats');
 
 const app = express();
 const port = 5000;
@@ -12,9 +12,11 @@ app.engine('hbs', handlebars({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', { cats: cats.getAllCats() });
 });
 
-
+app.get('/add-breed', (req, res) => {
+    res.render('addBreed');
+});
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`)); 
