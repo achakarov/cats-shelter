f6a543fb7e9566ed7c8b293d297ba9dbadb2b7ba
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-01-21 18:34:46 +0200

    Rendered home page without css

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const port = 5000;
 
 app.engine('hbs', handlebars({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+})
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`)); 
