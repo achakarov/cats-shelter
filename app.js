216d56dfc3e25b062dbec281a37605dca09fd3f1
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-01-21 21:31:42 +0200

    created logic to show search field only on home

--- app.js
+++ app.js
@@ -12,11 +12,13 @@ app.engine('hbs', handlebars({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    res.render('home', { cats: cats.getAllCats() });
+    res.render('home', { cats: cats.getAllCats(), isOnHomePage: true });
 });
 
 app.get('/add-breed', (req, res) => {
     res.render('addBreed');
 });
+
+
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`)); 
