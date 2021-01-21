194fd5925259b89c54e617d075e66e24f86223e3
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-01-21 21:42:05 +0200

    Finished creating breeds

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const path = require('path');
 const cats = require('./cats');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 5000;
 
 app.use('/static', express.static('public'));
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.engine('hbs', handlebars({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 
@@ -19,6 +20,11 @@ app.get('/add-breed', (req, res) => {
     res.render('addBreed');
 });
 
-
+app.post('/add-breed', (req, res) => {
+    console.log(req.body)
+    let catBreed = req.body.breed;
+    cats.addBreed(catBreed);
+    res.redirect('/');
+})
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`)); 
